3056cf81a0a1017067957f38ac01fc2e6493ff85
Tyler Christian <garrett.christian@mdteam.com>
2026-04-09 14:52:58 -0500

    Switch to setuptools-scm for automatic version from git tags

--- mkdocs_repo_docs/__init__.py
+++ mkdocs_repo_docs/__init__.py
@@ -1,3 +1,6 @@
 """mkdocs-repo-docs: Auto-discover and serve markdown files from anywhere in your repo."""
 
-__version__ = "0.1.0"
+try:
+    from mkdocs_repo_docs._version import version as __version__
+except ImportError:
+    __version__ = "0.0.0"
